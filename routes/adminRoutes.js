12a2116e62dc86a2c9f109dad95ca765f6c20c86
M.Durga Praneeth <muvvalapraneeth@gmail.com>
2026-08-07 20:19:09 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,14 +3,9 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const { getAllUsersController, getAllDoctorsController, changeAccountStatusController } = require('../controllers/adminCtrl');
 const router =express.Router()
 
-//GET METHOD || USERS
 
 router.get('/getAllUsers',authMiddleware,getAllUsersController);
-
-//GET METHOD || DOCTORS 
 router.get('/getAllDoctors',authMiddleware,getAllDoctorsController)
-
-//POST ACCOUNT STATUS
 
 router.post('/changeAccountStatus',authMiddleware,changeAccountStatusController)
 
